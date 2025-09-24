c92907e50146d7a96041435a1a2ebb7fab731405
Willy Saronamihardja <wsaronam@gmail.com>
2025-09-24 02:45:37 -0700

    updates
    - added a little frontend and backend updates
    - program not tested and not functional yet

--- backend/server.js
+++ backend/server.js
@@ -1,5 +1,33 @@
 import OpenAI from "openai";
+import express from "express";
+import cors from "cors";
+import dotenv from "dotenv";
 
 
+
+dotenv.config();
+const app = express();
+const port = 5000;
+
+app.use(cors());
+app.use(express.json());
 
 const client = new OpenAI({ apiKey: process.env.OPENAI_API_KEY });
+
+app.post("/api/respond", async (req, res) => {
+    try {
+        const {message} = req.body;
+
+        const response = await client.chat.completions.create({
+            messages: [
+                {role: "system", content: "You're simulating a helpful and polite customer support technician."}
+            ],
+        });
+    }
+    catch (err) {
+        console.error(error);
+    }
+});
+
+
+app.listen(port, () => console.log(`Backend running on http://localhost:${port}`));
--- frontend/src/Chat.js
+++ frontend/src/Chat.js
@@ -1,5 +1,40 @@
+import { useState } from "react";
+
+
+
+
 function Chat() {
+
+    const [input, setInput] = useState("");
+
+    
+    const sendMessage = async() => {
+        if (!input.trim()) return;
+
+        try {
+            const res = await axios.post("http://localhost:5000/api/respond", { message: input });
+        }
+        catch (err) {
+            console.error(err);
+        }
+
+        setInput("");
+    }
+
+
     return (
-        <h1>hello, world!</h1>
+        <div>
+            <h1>Customer Support Simulator</h1>
+
+            <input>
+                value={input}
+                {/* onChange={(e) => foo(e.target.value)} */}
+                placeholder="Type your message..."
+            </input>
+            <button onClick={sendMessage}>Send</button>
+        </div>
+    
     );
 }
+
+export default Chat;
